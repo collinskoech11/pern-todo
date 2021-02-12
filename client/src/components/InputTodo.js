102ff2fc3b2274ec3fba92d7d3ff0da24dbbfb7b
collinskoech11 <collinskoechck34@gmail.com>
2021-02-12 11:01:07 +0300

    Create EditTodo.js

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -2,10 +2,35 @@ import React, {Fragment, useState} from 'react';
 
 
 const InputTodo = () =>  {
+    const [description, setDescription] = useState =("");
+    const onSubmitForm = async e => {
+        e.preventDefault();
+        try {
+            const body = { description };
+            const response = await fetch("http://localhost:5000/todos", {
+                method: "POST",
+                headers: {"content-type": "appliation/json"},
+                body: JSON.stringify(body)
 
+            });
+
+            window.location ="/";
+        } catch (error) {
+            console.error(err.message);
+        }
+    }
     return (
         <Fragment>
-            
+             <h1 className="text-center mt-5">Pern Todo List</h1>
+            <form className="d-flex mt-5" onSubmit={onSubmitForm}>
+                <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+                />
+                <button className="btn btn-success">Add</button>
+            </form>
         </Fragment>
     )
 }
